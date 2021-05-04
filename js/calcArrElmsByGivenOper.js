8ed387d8d35f2b9764d9cd1b44cece151bad2237
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-04 13:00:09 +0400

    feat: Added the task description at the top of file

--- js/calcArrElmsByGivenOper.js
+++ js/calcArrElmsByGivenOper.js
@@ -1,3 +1,6 @@
+// 2. Write a function that calculates sum, difference, multiplication and division between given array elements
+//depending on passed operation symbol. Write appropriate function for each operation.
+
 function calcByFollowingOper(operator, inpArr) {
   let res = inpArr[0];
   switch (operator) {
