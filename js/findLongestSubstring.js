3f156ddd59728ed047a5e62fd9656f3f912be759
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-06 17:34:14 +0400

    fix: Has made fixes in the function with the Vazgen Harutyunyan

--- js/findLongestSubstring.js
+++ js/findLongestSubstring.js
@@ -2,30 +2,23 @@
 // character. If there are several,return the last one. Consider that all letters are lowercase.
 
 function findLongestSubstr(str) {
-  let resArr = [];
-
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < str.length; j++) {
-      if (str[i] === str[j] && i !== j) {
-        resArr.push(str.slice(i, j));
-      }
+  let res = "";
+  let resMax = "";
+  for (let char of str) {
+    if (char === " " || char === "'" || !res.includes(char)) {
+      res += char;
+    } else {
+      // console.log(res, 1);
+      res = res.slice(res.indexOf(char) + 1) + char;
+      // console.log(res, 2);
     }
+    resMax = resMax.length <= res.length ? res : resMax;
   }
-  for (let i = 0; i < resArr.length; i++) {
-    for (let j = 0; j < resArr[i].length; j++) {
-      for (let k = 0; k < resArr[i].length; k++) {
-        if (resArr[i][j] === resArr[i][k] && k !== j) {
-          resArr.splice(i, 1);
-        }
-      }
-    }
-  }
-  //   console.log(resArr.sort((a, b) => (a.length > b.length ? -1 : 1)));
-  return resArr.sort((a, b) => (a.length > b.length ? -1 : 1))[0];
+  return resMax;
 }
 
 console.log(
   findLongestSubstr(
-    "“there are no two words in the english language more harmful than 'good job'."
+    "there are no two words in the english language more harmful than 'good job'."
   )
 );
