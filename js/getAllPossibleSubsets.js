56560fa5a1b8e35ab9c67418c16973871e53628c
Samvel Demirtshyan <sam.demirtshyan@gmail.com>
2021-05-09 02:10:07 +0400

    fix: fixed the function

--- js/getAllPossibleSubsets.js
+++ js/getAllPossibleSubsets.js
@@ -7,21 +7,10 @@ function getPossibleSubsets(inpArr) {
   } else {
     let resArr = [];
     let tempArr = [];
-    for (let i = 0; i < inpArr.length; i++) {
-      for (let j = 0; j < inpArr.length; j++) {
-        for (let k = 0; k < inpArr.length; k++) {
-          if (tempArr.length < 3) {
-            if (i !== j && i !== k) {
-              tempArr.push(inpArr[i]);
-            } else if (j !== k && j !== i) {
-              tempArr.push(inpArr[j]);
-            } else if (k !== i && k !== j) {
-              tempArr.push(inpArr[k]);
-            }
-          } else {
-            resArr.push(tempArr);
-            tempArr = [];
-          }
+    for (let i = 0; i < inpArr.length - 2; i++) {
+      for (let j = i + 1; j < inpArr.length - 1; j++) {
+        for (let k = j + 1; k < inpArr.length; k++) {
+          resArr.push([inpArr[i], inpArr[j], inpArr[k]]);
         }
       }
     }
